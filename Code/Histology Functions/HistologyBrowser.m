5e8bbef9dd9f0593b80389bde1d84be639658c1d
Vincent Prevosto <prevosto@mit.edu>
2022-07-28 10:19:48 -0400

    streamlining

--- Code/Histology Functions/HistologyBrowser.m
+++ Code/Histology Functions/HistologyBrowser.m
@@ -2,109 +2,31 @@ function HistologyBrowser(histology_figure, ~, image_folder, coords_folder, imag
     use_already_downsampled_image, microns_per_pixel, microns_per_pixel_after_downsampling, gain)
 
 % display image and set up user controls for contrast change
-ud.show_original = 0;
-ud.adjusting_contrast = 0;
-ud.file_num = 1;
-ud.num_files = length(image_file_names);
-ud.save_folder = folder_processed_images;
-ud.coords_folder = coords_folder;
-ud.image_folder = image_folder;
-ud.microns_per_pixel = microns_per_pixel;
-ud.microns_per_pixel_after_downsampling = microns_per_pixel_after_downsampling;
-ud.gain = gain;
+ud = struct(...
+'show_original',0,...
+'adjusting_contrast',0,...
+'file_num',1,...
+'num_files',length(image_file_names),...
+'save_folder',folder_processed_images,...
+'coords_folder',coords_folder,...
+'image_folder',image_folder,...
+'image_file_names',{image_file_names},...
+'coords_file_names',{coords_file_names},...
+'microns_per_pixel',microns_per_pixel,...
+'microns_per_pixel_after_downsampling',microns_per_pixel_after_downsampling,...
+'gain',gain);
 
-% load histology image
-disp(['loading image ' num2str(ud.file_num) '...'])
+ud=AdjustHistologyImage(ud,use_already_downsampled_image);
 
-% load first image
-loadimage = imread(fullfile(ud.image_folder,image_file_names{ud.file_num}));
-ROI_location = fullfile(ud.coords_folder,coords_file_names{ud.file_num});
-ROI_values = readmatrix(ROI_location);
-ROI_table = readtable(ROI_location);
-
-if ~use_already_downsampled_image
-    % resize (downsample) image and ROI to reference atlas size
-    disp('adding ROI layer...')
-    disp('downsampling image...')
-    original_image_size = size(loadimage);
-    loadimage = imresize(loadimage, [round(original_image_size(1)*microns_per_pixel/microns_per_pixel_after_downsampling)  NaN]);
-    sz = size(squeeze(loadimage(:,:,1)));
-    ROI = zeros(sz);
-
-    % Root Lab Specific
-    %B = rot90(ROI);
-    %ROI = B;
-
-    if size(ROI_values,2) == 2
-        X = 1;
-        Y = 2;
-    else
-        X = find(strcmpi(ROI_table.Properties.VariableNames,'X'));
-        Y = find(strcmpi(ROI_table.Properties.VariableNames,'Y'));
-        ROI_values = ROI_values(2:end,:);
-    end
-
-    % ROI_values contains the data from the ROI coordinate file.
-    % If this file was obtained through 'multi-point' tool on FIJI and
-    % analyze->measure, then the X,Y data will be in the 6th and 7th col
-    x = round(ROI_values(:,X)*(sz(1,1)/original_image_size(1,1)));
-    y = round(ROI_values(:,Y)*(sz(1,2)/original_image_size(1,2)));
-    % Create binary ROI matrix 'image' and populate pixels with values if
-    % they represent labeled cell locations. If downsampled size causes two
-    % cell locations to overlap, move one pixel away until empty space
-    for i = 1:length(ROI_values(:,1))
-        if ROI(y(i),x(i)) == 0
-            ROI(y(i),x(i)) = 10;
-        else
-            if ROI(y(i)+1,x(i)) == 0
-                ROI(y(i)+1,x(i)) = 10;
-            else
-                ROI(y(i)-1,x(i)) = 10;
-            end
-        end
-    end
-
-    % Root Lab Specific
-    %B = rot90(ROI,3);
-    %ROI = B;
-
-else
-    % images are already downsampled to the atlas resolution 10um/px
-    disp('adding ROI layer...');
-    sz = size(squeeze(loadimage(:,:,1)));
-    ROI = zeros(sz);
-    x = round(ROI_values(:,6));
-    y = round(ROI_values(:,7));
-    for i = 1:length(ROI_values(:,1))
-        if ROI(y(i),x(i)) == 0
-            ROI(y(i),x(i)) = 10;
-        else
-            if ROI(y(i)+1,x(i)) == 0
-                ROI(y(i)+1,x(i)) = 10;
-            else
-                ROI(y(i)-1,x(i)) = 10;
-            end
-        end
-    end
-end
-ud.file_name_suffix = '_processed';
-ud.channel = min( 3, size(loadimage,3));
-original_image = loadimage(:,:,1:ud.channel)*gain;
-
-imshow(original_image);
+imshow(ud.original_image);
 title(['Adjusting channel ' num2str(ud.channel) ' on image ' num2str(ud.file_num) ' / ' num2str(ud.num_files)],...
     'color',[1==ud.channel 2==ud.channel 3==ud.channel])
 
-ud.original_image = original_image;
-ud.adjusted_image = original_image;
-
-% save pre-processed image and ROI
-imwrite(ud.adjusted_image, fullfile(ud.save_folder, [image_file_names{ud.file_num}(1:end-4) ud.file_name_suffix '.tif']))
-writematrix(ROI,fullfile(ud.save_folder, [image_file_names{ud.file_num}(1:end-4) ud.file_name_suffix '.csv']));
-
 set(histology_figure, 'UserData', ud);
 
-set(histology_figure, 'KeyPressFcn', @(histology_figure,keydata)HistologyHotkeyFcn(histology_figure, keydata, image_file_names, coords_file_names, use_already_downsampled_image));
+set(histology_figure, 'KeyPressFcn', @(histology_figure,keydata) ...
+    HistologyHotkeyFcn(histology_figure, keydata, image_file_names, ...
+                       coords_file_names, use_already_downsampled_image));
 
 fprintf(1, '\n Controls: adjust contrast for any RGB channel on any image \n \n');
 fprintf(1, 'space: adjust contrast for current channel / return to image-viewing mode \n');
@@ -258,7 +180,7 @@ end
 
 % show the image, unless in other viewing modes
 figure(fig)
-if ~(ud.adjusting_contrast || (strcmp(lower(keydata.Key),'e')&&ud.show_original) )
+if ~(ud.adjusting_contrast || (strcmpi(keydata.Key,'e')&&ud.show_original) )
     imshow(ud.adjusted_image)
 end
 title(['Adjusting channel ' num2str(ud.channel) ' on image ' num2str(ud.file_num) ' / ' num2str(ud.num_files)],...
@@ -266,3 +188,89 @@ title(['Adjusting channel ' num2str(ud.channel) ' on image ' num2str(ud.file_num
 
 set(fig, 'UserData', ud);
 
+% ----------------------------------
+%% Load and Adjust Histology Section
+% ----------------------------------
+function userData=AdjustHistologyImage(userData,use_ds_image)
+
+% load histology image
+disp(['loading image ' num2str(userData.file_num) '...'])
+
+% load first image
+loadimage = imread(fullfile(userData.image_folder,userData.image_file_names{userData.file_num}));
+ROI_location = fullfile(userData.coords_folder,userData.coords_file_names{userData.file_num});
+ROI_values = readmatrix(ROI_location);
+ROI_table = readtable(ROI_location);
+
+if ~use_ds_image
+    % resize (downsample) image and ROI to reference atlas size
+    disp('adding ROI layer...')
+    disp('downsampling image...')
+    original_image_size = size(loadimage);
+    loadimage = imresize(loadimage, [round(original_image_size(1)*userData.microns_per_pixel/userData.microns_per_pixel_after_downsampling)  NaN]);
+    sz = size(squeeze(loadimage(:,:,1)));
+    ROI = zeros(sz);
+
+    if size(ROI_values,2) == 2
+        X = 1;
+        Y = 2;
+    else
+        X = find(strcmpi(ROI_table.Properties.VariableNames,'X'));
+        Y = find(strcmpi(ROI_table.Properties.VariableNames,'Y'));
+        ROI_values = ROI_values(2:end,:);
+    end
+
+    % ROI_values contains the data from the ROI coordinate file.
+    % If this file was obtained through 'multi-point' tool on FIJI and
+    % analyze->measure, then the X,Y data will be in the 6th and 7th col
+    x = round(ROI_values(:,X)*(sz(1,1)/original_image_size(1,1)));
+    y = round(ROI_values(:,Y)*(sz(1,2)/original_image_size(1,2)));
+    % Create binary ROI matrix 'image' and populate pixels with values if
+    % they represent labeled cell locations. If downsampled size causes two
+    % cell locations to overlap, move one pixel away until empty space
+    for i = 1:length(ROI_values(:,1))
+        if ROI(y(i),x(i)) == 0
+            ROI(y(i),x(i)) = 10;
+        else
+            if ROI(y(i)+1,x(i)) == 0
+                ROI(y(i)+1,x(i)) = 10;
+            else
+                ROI(y(i)-1,x(i)) = 10;
+            end
+        end
+    end
+
+else
+    % images are already downsampled to the atlas resolution 10um/px
+    disp('adding ROI layer...');
+    sz = size(squeeze(loadimage(:,:,1)));
+    ROI = zeros(sz);
+    x = round(ROI_values(:,6));
+    y = round(ROI_values(:,7));
+    for i = 1:length(ROI_values(:,1))
+        if ROI(y(i),x(i)) == 0
+            ROI(y(i),x(i)) = 10;
+        else
+            if ROI(y(i)+1,x(i)) == 0
+                ROI(y(i)+1,x(i)) = 10;
+            else
+                ROI(y(i)-1,x(i)) = 10;
+            end
+        end
+    end
+end
+userData.file_name_suffix = '_processed';
+userData.channel = min( 3, size(loadimage,3));
+original_image = loadimage(:,:,1:userData.channel)*gain;
+
+% imshow(original_image);
+% title(['Adjusting channel ' num2str(ud.channel) ' on image ' num2str(ud.file_num) ' / ' num2str(ud.num_files)],...
+%     'color',[1==ud.channel 2==ud.channel 3==ud.channel])
+
+[userData.original_image,userData.adjusted_image] = deal(original_image);
+
+% save pre-processed image and ROI
+imwrite(userData.adjusted_image, fullfile(userData.save_folder, ...
+    [userData.image_file_names{userData.file_num}(1:end-4) userData.file_name_suffix '.tif']))
+writematrix(ROI,fullfile(userData.save_folder, ...
+    [userData.image_file_names{userData.file_num}(1:end-4) userData.file_name_suffix '.csv']));
